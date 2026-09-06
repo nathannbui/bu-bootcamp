114062130b3e1fcc10f306ac7021136967173004
Nathan Bui <nathanbui12345@gmail.com>
2026-09-06 18:34:21 -0400

    Small Change

--- ContactManager.java
+++ ContactManager.java
@@ -2,4 +2,7 @@ public class ContactManager {
     public ContactManager() {
         System.out.println("Contact Manager initialized.");
     }
+    public void addContact(Contact contact) {
+        System.out.println("Contact added.");
+    }
 }
